f30e08bbb5073ee6b3c261b534d07aa32af414ff
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-10-07 19:15:52 -0300

    TG: Created Router for the 'time sheets' endpoints

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -1,1 +1,10 @@
-// Fabricio and Martin
+const express = require('express');
+const fileSystem = require('fs');
+const timeSheets = require('../data/time-sheets.json');
+
+const router = express.Router();
+
+router.get('/getAll', (req, res) => {
+    res.send(timeSheets);
+});
+module.exports = router;
