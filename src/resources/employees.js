1ae2a3616e9a73a52dbe8ad7e180d925edcdf7f0
matiasraviola <matiasaraviola@gmail.com>
2022-10-10 19:31:17 -0300

    TG-XX: put employees corrections done

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,40 +1,32 @@
 const express = require('express');
+const fs = require('fs');
 const employees = require('../data/employees.json');
+
 const employeesRouter = express.Router();
-const fs = require ('fs')
-employeesRouter.get('/getAll', (req,res) => {
-        res.send(employees)
-})
-employeesRouter.get('/getEmployee/:id',(req,res) => {
-    const employeeId = req.params.id;
-   const findEmployee = employees.find((employee) => employee.id == employeeId);
-   if(findEmployee){
-    res.send(findEmployee)
-    }else{
+employeesRouter.get('/getAll', (req, res) => {
+  res.send(employees);
+});
+employeesRouter.get('/getEmployee/:id', (req, res) => {
+  const employeeId = req.params.id;
+  const findEmployee = employees.find((employee) => employee.id === employeeId);
+  if (findEmployee) {
+    res.send(findEmployee);
+  } else {
     res.send("Employee dosen't exist");
+  }
+});
+employeesRouter.put('/editEmployee/:id', (req, res) => {
+  const employeeId = req.params.id;
+  const employeeEdit = req.body;
+  const employeeFilter = employees.filter((employee) => `${employee.id}` !== employeeId);
+  employeeFilter.push(employeeEdit);
+  fs.writeFile('src/data/employees.json', JSON.stringify(employeeFilter), (err) => {
+    if (err) {
+      res.status(400).send('Error');
+    } else {
+      res.send('user updating');
     }
-})
-employeesRouter.put('/editEmployee/:id',(req,res) => {
-    const employeeId = req.params.id;
-    const employeeEdit = req.body;
-    const employeeFilter = employees.filter((employee) => `${employee.id}` !== employeeId)
-    console.log(employeeFilter)
-    employeeFilter.push(employeeEdit);
-    fs.writeFile('src/data/employees.json', JSON.stringify(employeeFilter),(err) => {
-        if(err){
-            res.status(400).send('Error')
-        }else{
-            res.send('user updating')
-        }
-    })
-    // if (employeeFilter){
-    //     if(employeeEdit.id != "" || employeeEdit.first_name != "" || employeeEdit.last_name != "" || employeeEdit.email != "" || employeeEdit.password != "" || employeeEdit.phone != ""){
-    // res.send(employeeFilter);
-    //     }else{
-    //         res.send("body incomplete")
-    //     }
-    // }
-    // console.log(employeeEdit.last_name)
-})
-employeesRouter.get('/')
+  });
+});
+
 module.exports = employeesRouter;
