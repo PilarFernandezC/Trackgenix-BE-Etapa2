344eda94b7f61e455db311f23d14bbe8d6596f91
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-10-10 01:22:50 -0300

    TG: /getAll and /getById endpoints created for @ /time-sheets

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -1,10 +1,28 @@
 const express = require('express');
-const fileSystem = require('fs');
+// const fileSystem = require('fs');
 const timeSheets = require('../data/time-sheets.json');
 
 const router = express.Router();
 
 router.get('/getAll', (req, res) => {
-    res.send(timeSheets);
+  res.send(timeSheets);
 });
+
+router.get('/getById', (req, res) => {
+  const queryId = req.query.id;
+  if (queryId) {
+    const targetId = parseInt(queryId, 10);
+    const targetTS = timeSheets.filter((current) => current.id === targetId).pop();
+    if (targetTS) {
+      res.send(targetTS);
+    } else {
+      res.status = 404;
+      res.send(`Error: Time sheet with id=${targetId} not found.`);
+    }
+  } else {
+    res.status = 400;
+    res.send('Error: invalid request.');
+  }
+});
+
 module.exports = router;
