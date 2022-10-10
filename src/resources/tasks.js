4e452346f5156c191ff4b3196af82d4a5342c4c3
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-10-10 16:59:04 -0300

    TG: Added /getById endpoint to tasks.js

--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -7,4 +7,15 @@ router.get('/getAll', (req, res) => {
   res.send(taskList);
 });
 
+router.get('/getById', (req, res) => {
+  const taskIndex = parseInt(req.query.id, 10) - 1;
+  const selectedTask = taskList[taskIndex];
+  if (selectedTask) {
+    res.send(selectedTask);
+  } else {
+    res.status(404);
+    res.send(`Error: No task with id=${req.query.id} found.`);
+  }
+});
+
 module.exports = router;
