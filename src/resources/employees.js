9c13927dd14354480c86b855d1606f006481905d
PilarFernandezC <pilarfernandezc98@gmail.com>
2022-10-09 22:40:18 -0300

    TG: delete get test and add post method

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
+const fs = require('fs');
+
 const employees = require('../data/employees.json');
 
 const router = express.Router();
 
-router.get('/getAll', (req, res) => {
-  res.send(employees);
+router.post('/add', (req, res) => {
+  const newEmployee = req.body;
+  employees.push(newEmployee);
+  fs.writeFile('src/data/employees.json', JSON.stringify(employees), (err) => {
+    if (err) {
+      res.send('Failed creating new user');
+    } else {
+      res.send('User created successfully');
+    }
+  });
 });
 
 module.exports = router;
