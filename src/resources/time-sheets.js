5628a716645e1f2df679f4183a53bb975a1a76b8
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-10-10 09:38:03 -0300

    TG: time-sheets/editBy endpoint created

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const fileSystem = require('fs');
+const fileSystem = require('fs');
 const timeSheets = require('../data/time-sheets.json');
 
 const router = express.Router();
@@ -24,5 +24,37 @@ router.get('/getById', (req, res) => {
     res.send('Error: invalid request.');
   }
 });
-
+router.put('/editById', (req, res) => {
+  const {
+    id: reqId,
+    date: newDate,
+    description: newDescription,
+    task: newTask,
+  } = req.body;
+  const indexOfTS = timeSheets.findIndex((element) => element.id === parseInt(reqId, 10));
+  if (indexOfTS !== -1) {
+    const {
+      id,
+      date: oldDate,
+      description: oldDescription,
+      task: oldTask,
+    } = timeSheets[indexOfTS];
+    const updatedTimesheet = {
+      id,
+      date: newDate || oldDate,
+      description: newDescription || oldDescription,
+      task: newTask || oldTask,
+    };
+    timeSheets[indexOfTS] = updatedTimesheet;
+    fileSystem.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheets, null, 4), (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(`Timesheet modified\n ${JSON.stringify(updatedTimesheet, null, 4)}`);
+      }
+    });
+  } else {
+    res.send('Modify error: Timesheet not found');
+  }
+});
 module.exports = router;
