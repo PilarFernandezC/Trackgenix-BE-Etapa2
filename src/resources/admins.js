dfc5f949b296cb6dd895ec197005eccb082dae98
Ernesto <ejubessone@gmail.com>
2022-10-11 14:03:08 -0300

    TG: Create admin endpoint updated

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -27,13 +27,13 @@ router.post('/createAdmin', (req, res) => {
     password: req.body.password,
   };
   admins.push(newAdmin);
-  fs.writeFile('src/data/admins.json', JSON.stringify(admins), (err) => {
-    if (err) {
-      res.send('ERROR');
-    } else {
-      res.send(`Admin ${req.body.email} created`);
+  fs.writeFile('src/data/admins.json', JSON.stringify(admins, null, 4), (error) => {
+    if (error) {
+      res.status(400).json({ msg: 'ERROR!' });
     }
+    res.send(`Admin ${req.body.email} created`);
   });
 });
+
 // final footer
 module.exports = router;
