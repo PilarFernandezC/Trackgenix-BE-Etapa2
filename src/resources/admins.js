a41cbf4f2a0d7a67dae5eaa56636b5d76f8a9596
Ernesto <ejubessone@gmail.com>
2022-10-11 15:25:30 -0300

    TG: Replaced .filter with .some and changed error message on create admin

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -9,7 +9,7 @@ router.get('/getAllAdmins', (req, res) => res.json(admins));
 
 // get admin by id - Bessone
 router.get('/:id', (req, res) => {
-  const adminFound = admins.find((admin) => admin.id === parseInt(req.params.id, 10));
+  const adminFound = admins.some((admin) => admin.id === parseInt(req.params.id, 10));
   if (adminFound) {
     res.json(admins.filter((admin) => admin.id === parseInt(req.params.id, 10)));
   } else {
@@ -29,7 +29,7 @@ router.post('/createAdmin', (req, res) => {
   admins.push(newAdmin);
   fs.writeFile('src/data/admins.json', JSON.stringify(admins, null, 4), (error) => {
     if (error) {
-      res.status(400).json({ msg: 'ERROR!' });
+      res.status(400).json({ msg: 'ERROR! Could not create an Admin' });
     }
     res.send(`Admin ${req.body.email} created`);
   });
