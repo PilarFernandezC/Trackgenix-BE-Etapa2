673bddb1caf459864be73a9c7ba5f2f0e1f45924
matiasraviola <matiasaraviola@gmail.com>
2022-10-11 15:27:13 -0300

    TG-XX: some corrections about the parameters and syntax

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -7,8 +7,9 @@ employeesRouter.get('/getAll', (req, res) => {
   res.send(employees);
 });
 employeesRouter.get('/getEmployee/:id', (req, res) => {
-  const employeeId = req.params.id;
+  const employeeId = parseInt(req.params.id);
   const findEmployee = employees.find((employee) => employee.id === employeeId);
+  console.log(findEmployee)
   if (findEmployee) {
     res.send(findEmployee);
   } else {
@@ -20,11 +21,11 @@ employeesRouter.put('/editEmployee/:id', (req, res) => {
   const employeeEdit = req.body;
   const employeeFilter = employees.filter((employee) => `${employee.id}` !== employeeId);
   employeeFilter.push(employeeEdit);
-  fs.writeFile('src/data/employees.json', JSON.stringify(employeeFilter), (err) => {
+  fs.writeFile('src/data/employees.json', JSON.stringify(employeeFilter,null,4), (err) => {
     if (err) {
       res.status(400).send('Error');
     } else {
-      res.send('user updating');
+      res.send('Employee Updated');
     }
   });
 });
