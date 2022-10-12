d15ed5d408625ec7059a633854097ba0e47a7d22
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-10-12 20:17:44 -0300

    TG: fixed case issue. #discussion_r993466714

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/time-sheets', timeSheetsRouter);
+app.use('/timeSheets', timeSheetsRouter);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
