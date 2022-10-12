a58b0bfa4017df62afe3c84c31add98e676b49f9
martinzancocchia <tinchozan@gmail.com>
2022-10-12 16:23:44 -0300

    TG: corrections applied

--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -4,33 +4,38 @@ const tasks = require('../data/tasks.json');
 
 const router = express.Router();
 
-router.post('/addNewTask', (req, res) => {
-    const newTask = req.body;
-    tasks.push(newTask);
-    fileSystem.writeFile('src/data/tasks.json', JSON.stringify(tasks, null, 4), (err) => {
+router.post('/add', (req, res) => {
+  const newTask = req.body;
+  tasks.push(newTask);
+  fileSystem.writeFile(
+    'src/data/tasks.json',
+    JSON.stringify(tasks, null, 4),
+    (err) => {
       if (err) {
         res.send('Cannot save new task');
       } else {
         res.send(`Task added \n ${JSON.stringify(newTask)}`);
       }
-    });
-  });
+    },
+  );
+});
 
-  router.get('/getTaskByDesc', (req, res) => {
-    const queryDescription = req.query.description;
-    if (queryDescription) {
-      const targetDesc = (queryDescription);
-      const targetTask = tasks.filter((current) => current.description === targetDesc).pop();
-      if (targetTask) {
-        res.send(targetTask);
-      } else {
-        res.status = 404;
-        res.send(`Error, task with description= ${targetDesc} not found.`);
-      }
+router.get('/getByDescription/:description', (req, res) => {
+  const taskDescription = req.params.description;
+  if (taskDescription) {
+    const targetDesc = taskDescription;
+    const targetTask = tasks
+      .filter((current) => current.description === targetDesc);
+    if (targetTask.length > 0) {
+      res.send(targetTask);
     } else {
-      res.status = 400;
-      res.send('Error, invalid request.');
+      res.status = 404;
+      res.send(`ERROR, task with description: ${targetDesc} not found.`);
     }
-  });
+  } else {
+    res.status = 400;
+    res.send('ERROR, invalid request.');
+  }
+});
 
-  module.exports = router;
+module.exports = router;
