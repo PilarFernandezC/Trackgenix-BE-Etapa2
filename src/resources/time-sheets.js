4b245e87eb2f85ef8c6f154d5089b3446efc07f2
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-10-12 20:33:29 -0300

    TG: /getById and /editById endpoints now receive id as URI path prams. #pullrequestreview-1139202820

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -8,8 +8,8 @@ router.get('/getAll', (req, res) => {
   res.send(timeSheets);
 });
 
-router.get('/getById', (req, res) => {
-  const queryId = req.query.id;
+router.get('/getById/:id', (req, res) => {
+  const queryId = req.params.id;
   if (queryId) {
     const targetId = parseInt(queryId, 10);
     const targetTS = timeSheets.filter((current) => current.id === targetId).pop();
@@ -24,37 +24,43 @@ router.get('/getById', (req, res) => {
     res.send('Error: invalid request.');
   }
 });
-router.put('/editById', (req, res) => {
-  const {
-    id: reqId,
-    date: newDate,
-    description: newDescription,
-    task: newTask,
-  } = req.body;
-  const indexOfTS = timeSheets.findIndex((element) => element.id === parseInt(reqId, 10));
-  if (indexOfTS !== -1) {
+router.put('/editById/:id', (req, res) => {
+  const reqId = req.params.id;
+  if (reqId) {
     const {
-      id,
-      date: oldDate,
-      description: oldDescription,
-      task: oldTask,
-    } = timeSheets[indexOfTS];
-    const updatedTimesheet = {
-      id,
-      date: newDate || oldDate,
-      description: newDescription || oldDescription,
-      task: newTask || oldTask,
-    };
-    timeSheets[indexOfTS] = updatedTimesheet;
-    fileSystem.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheets, null, 4), (err) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(`Timesheet modified\n ${JSON.stringify(updatedTimesheet, null, 4)}`);
-      }
-    });
+      id: newId,
+      date: newDate,
+      description: newDescription,
+      task: newTask,
+    } = req.body;
+    const indexOfTS = timeSheets.findIndex((element) => element.id === parseInt(reqId, 10));
+    if (indexOfTS !== -1) {
+      const {
+        id: oldId,
+        date: oldDate,
+        description: oldDescription,
+        task: oldTask,
+      } = timeSheets[indexOfTS];
+      const updatedTimesheet = {
+        id: newId || oldId,
+        date: newDate || oldDate,
+        description: newDescription || oldDescription,
+        task: newTask || oldTask,
+      };
+      timeSheets[indexOfTS] = updatedTimesheet;
+      fileSystem.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheets, null, 4), (err) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send(`Timesheet modified\n ${JSON.stringify(updatedTimesheet, null, 4)}`);
+        }
+      });
+    } else {
+      res.send(`Error: Timesheet id=${reqId} not found`);
+    }
   } else {
-    res.send('Modify error: Timesheet not found');
+    res.status = 400;
+    res.send('Error: no id specified.');
   }
 });
 function orderDate(dateStr, refDateStr) {
