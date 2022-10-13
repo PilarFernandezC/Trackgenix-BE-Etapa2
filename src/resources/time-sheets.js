c312bc7a752f9ace082820b55ad03592458eed60
martinzancocchia <tinchozan@gmail.com>
2022-10-12 21:01:48 -0300

    TG: querys have been replaced by parameters on endpoints

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -5,11 +5,13 @@ const timeSheets = require('../data/time-sheets.json');
 const router = express.Router();
 
 router.get('/getByDate', (req, res) => {
-  const queryDate = req.query.date;
-  if (queryDate) {
-    const targetDate = (queryDate);
-    const targetTS = timeSheets.filter((current) => current.date === targetDate).pop();
-    if (targetTS) {
+  const tSDate = req.query.date;
+  if (tSDate) {
+    const targetDate = tSDate;
+    const targetTS = timeSheets.filter(
+      (current) => current.date === targetDate,
+    );
+    if (targetTS.length > 0) {
       res.send(targetTS);
     } else {
       res.status = 404;
@@ -21,12 +23,14 @@ router.get('/getByDate', (req, res) => {
   }
 });
 
-router.get('/getByDesc', (req, res) => {
-  const queryDescription = req.query.description;
-  if (queryDescription) {
-    const targetDesc = (queryDescription);
-    const targetTS = timeSheets.filter((current) => current.description === targetDesc).pop();
-    if (targetTS) {
+router.get('/getByDescription/:description', (req, res) => {
+  const tSDescription = req.params.description;
+  if (tSDescription) {
+    const targetDesc = tSDescription;
+    const targetTS = timeSheets.filter(
+      (current) => current.description === targetDesc,
+    );
+    if (targetTS.length > 0) {
       res.send(targetTS);
     } else {
       res.status = 404;
@@ -41,25 +45,41 @@ router.get('/getByDesc', (req, res) => {
 router.post('/add', (req, res) => {
   const newTimeSheet = req.body;
   timeSheets.push(newTimeSheet);
-  fileSystem.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheets, null, 4), (err) => {
-    if (err) {
-      res.send('Cannot save new user');
-    } else {
-      res.send(`User created\n ${JSON.stringify(newTimeSheet)}`);
-    }
-  });
+  fileSystem.writeFile(
+    'src/data/time-sheets.json',
+    JSON.stringify(timeSheets, null, 4),
+    (err) => {
+      if (err) {
+        res.send('Cannot save new user');
+      } else {
+        res.send(`User created\n ${JSON.stringify(newTimeSheet)}`);
+      }
+    },
+  );
 });
 
-router.delete('/deleteById', (req, res) => {
-  const timeSheetsId = parseInt(req.query.id, 10);
-  const timeSheetsFilter = timeSheets.filter((timeSheet) => timeSheet.id !== timeSheetsId);
-  fileSystem.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheetsFilter, null, '\n'), (err) => {
-    if (err) {
-      res.send('Cannot found the item');
-    } else {
-      res.send(`Item id=${timeSheetsId} deleted\n${JSON.stringify(timeSheets, null, 4)}`);
-    }
-  });
+router.delete('/deleteById/:id', (req, res) => {
+  const timeSheetsId = parseInt(req.params.id, 10);
+  const timeSheetsFilter = timeSheets.filter(
+    (timeSheet) => timeSheet.id !== timeSheetsId,
+  );
+  fileSystem.writeFile(
+    'src/data/time-sheets.json',
+    JSON.stringify(timeSheetsFilter, null, 4),
+    (err) => {
+      if (err) {
+        res.send('Cannot found the item');
+      } else {
+        res.send(
+          `Item id: ${timeSheetsId} deleted\n${JSON.stringify(
+            timeSheets,
+            null,
+            4,
+          )}`,
+        );
+      }
+    },
+  );
 });
 
 module.exports = router;
