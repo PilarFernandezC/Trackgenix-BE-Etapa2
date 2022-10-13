b1b6dd250097f458e4cfaf1db770a6c7163ea7f7
PilarFernandezC <pilarfernandezc98@gmail.com>
2022-10-13 14:24:33 -0300

    TG: make corrections with the comments of the pr

--- src/resources/super-admins.js
+++ src/resources/super-admins.js
@@ -1,15 +1,13 @@
 const express = require('express');
-
 const fs = require('fs');
-
 const superAdmins = require('../data/super-admins.json');
 
 const router = express.Router();
 
 router.delete('/delete/:id', (req, res) => {
   const superAdminId = parseInt(req.params.id, 10);
-  const superAdminDelete = superAdmins.some((superAdmin) => superAdmin.id === superAdminId);
-  if (superAdminDelete) {
+  const superAdminToDelete = superAdmins.some((superAdmin) => superAdmin.id === superAdminId);
+  if (superAdminToDelete) {
     const remainingSuperAdmins = superAdmins.filter((superAdmin) => superAdmin.id !== superAdminId);
     fs.writeFile('src/data/super-admins.json', JSON.stringify(remainingSuperAdmins, null, 4), (err) => {
       if (err) {
