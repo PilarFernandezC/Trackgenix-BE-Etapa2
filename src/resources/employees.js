e122193a92c83b5cd74a62a4cfe0df3c90b0f5fd
PilarFernandezC <pilarfernandezc98@gmail.com>
2022-10-13 14:33:10 -0300

    TG: make corrections with the comments of the pr

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const fs = require('fs');
-
 const employees = require('../data/employees.json');
 
 const router = express.Router();
@@ -11,23 +9,23 @@ router.post('/add', (req, res) => {
   employees.push(newEmployee);
   fs.writeFile('src/data/employees.json', JSON.stringify(employees, null, 4), (err) => {
     if (err) {
-      res.send('Failed creating new user');
+      res.send('Failed creating new employee');
     } else {
-      res.send('User created successfully');
+      res.send('Employee created successfully');
     }
   });
 });
 
 router.delete('/delete/:id', (req, res) => {
   const employeeId = parseInt(req.params.id, 10);
-  const employeeDelete = employees.some((employee) => employee.id === employeeId);
-  if (employeeDelete) {
+  const employeeToDelete = employees.some((employee) => employee.id === employeeId);
+  if (employeeToDelete) {
     const remainingEmployees = employees.filter((employee) => employee.id !== employeeId);
     fs.writeFile('src/data/employees.json', JSON.stringify(remainingEmployees, null, 4), (err) => {
       if (err) {
-        res.send('Failed deleting user');
+        res.send('Failed deleting employee');
       } else {
-        res.send('User deleted successfully');
+        res.send('Employee deleted successfully');
       }
     });
   } else {
@@ -35,7 +33,7 @@ router.delete('/delete/:id', (req, res) => {
   }
 });
 
-router.get('/filter/:first_name', (req, res) => {
+router.get('/filterByName/:first_name', (req, res) => {
   const firstName = req.params.first_name;
   const filterEmployees = employees.filter((employee) => employee.first_name.includes(firstName));
   if (filterEmployees.length > 0) {
