ca203a4f201519a537983425ed83ee6f4630e12c
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-10-12 21:23:02 -0300

    TG: /getById and /deleteById endpoints now take id as a URL path param. No data dump is returned upon deletion.

--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fs = require('fs')
+const fs = require('fs');
 const taskList = require('../data/tasks.json');
 
 const router = express.Router();
@@ -8,37 +8,39 @@ router.get('/getAll', (req, res) => {
   res.send(taskList);
 });
 
-router.get('/getById', (req, res) => {
-  const taskIndex = parseInt(req.query.id, 10) - 1;
+router.get('/getById/:id', (req, res) => {
+  const taskIndex = parseInt(req.params.id, 10) - 1;
   const selectedTask = taskList[taskIndex];
   if (selectedTask) {
     res.send(selectedTask);
   } else {
     res.status(404);
-    res.send(`Error: No task with id=${req.query.id} found.`);
+    res.send(`Error: No task with id=${req.params.id} found.`);
   }
 });
 
-router.delete('/deleteById', (req, res) => {
-    const taskId = parseInt(req.query.id, 10);
-    if (taskList.some((task) => task.id == taskId)) {
+router.delete('/deleteById/:id', (req, res) => {
+  const taskId = parseInt(req.params.id, 10);
+  if (!(Number.isNaN(taskId))) {
+    if (taskList.some((task) => task.id === taskId)) {
       const updatedTaskList = taskList.filter((task) => task.id !== taskId);
-      const payload = JSON.stringify(updatedTaskList, null, 2);
+      const payload = JSON.stringify(updatedTaskList, null, 4);
       fs.writeFile('src/data/tasks.json', payload, (err) => {
-        if(err){
+        if (err) {
           res.status(500);
-          res.send('Error: cannnot write to server files system. Nothing changed.')
-        } else{
-          res.send([
-            `Task id=${req.query.id} successfully deleted.`,
-            payload
-          ].join('\n'));
+          res.send('Error: cannnot write to server files system. Nothing changed.');
+        } else {
+          res.send(`Task id=${req.params.id} successfully deleted.`);
         }
-      })
+      });
     } else {
       res.status(404);
-      res.send(`Error: No task with id=${req.query.id} found.`);
+      res.send(`Error: No task with id=${req.params.id} found.`);
     }
-})
+  } else {
+    res.status(400);
+    res.send('Error: No id parameter provided.');
+  }
+});
 
 module.exports = router;
