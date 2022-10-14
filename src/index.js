1613591a41b1ed55474f0a2580db1c9f2f27278b
Sabrina Reckinger <sabrina.reckinger@gmail.com>
2022-10-14 13:24:58 -0300

    TG: add const proyectRouter

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const tasksRouter = require('./resources/tasks');
 const employees = require('./resources/employees');
 const timeSheetsRouter = require('./resources/time-sheets');
 const superAdmin = require('./resources/super-admins');
-
+const projectsRouter = require('./resources/projects')
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/projects", projectRouter);
-app.use("/tasks", tasksRouter);
+app.use('/projects', projectsRouter);
+app.use('/tasks', tasksRouter);
 app.use('/timeSheets', timeSheetsRouter);
 app.use('/employees', employees);
 app.use('/superAdmin', superAdmin);
