525bb14f5fc8be3fe7ec3af60728b8518bb0e77e
matiasraviola <matiasaraviola@gmail.com>
2022-10-15 12:11:48 -0300

    TG-07: master merged and superAdmin.js from models edited

---
+++ src/models/SuperAdmin.js
@@ -0,0 +1,11 @@
+import { number, string } from "joi";
+import mongoose from "mongoose";
+const {Schema} = mongoose;
+const superAdmin = new Schema({
+    id:{type:Number, requiered:true},
+    name:{type:String, required:true},
+    lastName:{type:String, required:true},
+    email:{type:String, required:true},
+    password:{type:String, required:true},
+});
+export default mongoose.model('superAdmin',superAdminSchema);
