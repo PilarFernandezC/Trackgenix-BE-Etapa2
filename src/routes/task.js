4d6e14deb6713e75e26404e6b82c99a7f0a4d77b
martinzancocchia <tinchozan@gmail.com>
2022-10-16 21:39:06 -0300

    TG: create task endpoint removed

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,17 +1,17 @@
 import express from 'express';
 // import validations from '../validations/task';
 import {
-  deleteTask, editTask, getAll, getTaskById, createTask
+  deleteTask, editTask, getAll, getTaskById,
 } from '../controllers/task';
-import Task from '../models/Task';
+// import { updateTaskValidation } from '../validations/task';
 
 const router = express.Router();
 
 router
-    .delete('/:id', deleteTask)
-    .post('/', createTask)
-    .get('/:id', getTaskById)
-    .put('/:id', editTask)
-    .get('/', getAll);
+  .delete('/:id', deleteTask)
+//   .post('/', createTask)
+  .get('/:id', getTaskById)
+  .put('/:id', editTask)
+  .get('/', getAll);
 
 export default router;
