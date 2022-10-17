6e440c878bd9027dc59d07134f4d3f0d24f1fb94
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-10-17 10:03:17 -0300

    TG-11: MongoDB controller function for filtering has been created.

--- src/controllers/employee.js
+++ src/controllers/employee.js
@@ -34,12 +34,30 @@ async function createEmployeeMongo(req, res) {
   }
 }
 
+async function fileterEmployeesMongo(req, res) {
+  try {
+    const filteredEmployees = await Employee.find(req.body);
+    res.status(200).json({
+      message: 'List of employees that match the request criteria successfully retrieved.',
+      data: filteredEmployees,
+      error: false,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: `Failed to retrieve documents in database: ${error.message}`,
+      data: undefined,
+      error: true,
+    });
+  }
+}
+
 // const employeeLocalDBController = {
 //   create: createEmployeeLocal,
 // };
 
 const employeeDBController = {
   create: createEmployeeMongo,
+  filter: fileterEmployeesMongo,
 };
 
 module.exports = employeeDBController;
