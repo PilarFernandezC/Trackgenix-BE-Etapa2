59de1428b37a5539a2ab43cc1075ff7ec91d58b6
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-10-17 09:46:45 -0300

    TG-11: Mongo Employee model created.

---
+++ src/models/Employee.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const employeeSchema = new Schema({
+  name: { type: String, required: true },
+  lastName: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const Employee = mongoose.model('Employee', employeeSchema);
+
+module.exports = Employee;
