816365fcde1c444cacebd4593151d55affbb69d0
matiasraviola <matiasaraviola@gmail.com>
2022-10-17 16:08:37 -0300

    TG-07: lint corrections

--- src/validations/superAdmin.js
+++ src/validations/superAdmin.js
@@ -6,7 +6,7 @@ const createValidation = (req, res, next) => {
     lastName: Joi.string().min(3).pattern(/^[\p{L}]+$/u).required(),
     email: Joi.string().email().required(),
     password: Joi.string().pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/).required(),
-});
+  });
   const validation = superAdminValidation.validate(req.body);
   if (validation.error) {
     return res.status(400).json({
