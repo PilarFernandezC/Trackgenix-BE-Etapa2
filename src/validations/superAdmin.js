794b4ec48ce8f250551fdbd41047477e8d3d348b
matiasraviola <matiasaraviola@gmail.com>
2022-10-17 16:08:07 -0300

    TG-07: password regex corrections

--- src/validations/superAdmin.js
+++ src/validations/superAdmin.js
@@ -5,8 +5,8 @@ const createValidation = (req, res, next) => {
     name: Joi.string().min(3).pattern(/^[\p{L}]+$/u).required(),
     lastName: Joi.string().min(3).pattern(/^[\p{L}]+$/u).required(),
     email: Joi.string().email().required(),
-    password: Joi.string().pattern(/^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{8,}$/).required(),
-  });
+    password: Joi.string().pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/).required(),
+});
   const validation = superAdminValidation.validate(req.body);
   if (validation.error) {
     return res.status(400).json({
