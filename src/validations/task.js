8c8fae7097d3977906fba104f76d8fbfafe6a90f
martinzancocchia <tinchozan@gmail.com>
2022-10-16 21:36:43 -0300

    TG: validations added

---
+++ src/validations/task.js
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+
+const taskValidateUpdate = Joi.object({
+  description: Joi.string().valid(
+    'BE',
+    'FE',
+  ),
+});
+
+const updateTaskValidation = (req, res, next) => {
+  const validation = taskValidateUpdate.validate(req.body);
+  if (validation.error) {
+    return res.status(400).json({
+      msg: 'There was a validation error:',
+      data: validation.error.details.map((x) => x.message).join(', '),
+      error: true,
+    });
+  } return next();
+};
+
+export default { updateTaskValidation };
