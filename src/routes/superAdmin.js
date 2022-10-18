18d39ff22662ec5f924b864c7d3b1113df54ded6
matiasraviola <matiasaraviola@gmail.com>
2022-10-18 16:52:56 -0300

    TG-07: getAll filter done

--- src/routes/superAdmin.js
+++ src/routes/superAdmin.js
@@ -5,7 +5,6 @@ import superAdminValidation from '../validations/superAdmin';
 const router = express.Router();
 
 router.get('/', superAdminControllers.getAll);
-// router.get('/params',superAdminControllers.filtSuperAdmin);
 router.post('/', superAdminValidation.createValidation, superAdminControllers.create);
 
 export default router;
