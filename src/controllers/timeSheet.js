3ef52b735fb163fe54861d0e0a2d6e37639a8362
Ernesto Bessone <ejubessone@gmail.com>
2022-10-18 20:19:25 -0300

    TG-14: getAll endpoint added

--- src/controllers/timeSheet.js
+++ src/controllers/timeSheet.js
@@ -1,8 +1,8 @@
-import Models from '../models/TimeSheet';
+import TimesheetModel from '../models/TimeSheet';
 
 const createTimesheet = async (req, res) => {
   try {
-    const newTimesheet = new Models({
+    const newTimesheet = new TimesheetModel({
       description: req.body.description,
       date: req.body.date,
       task: req.body.task,
@@ -19,6 +19,42 @@ const createTimesheet = async (req, res) => {
   }
 };
 
+const getAllTimesheets = async (req, res) => {
+  const timesheetArray = Object.keys(req.query);
+  try {
+    const TimesheetFound = await TimesheetModel.find();
+    if (!TimesheetFound) {
+      return res.status(404).json({
+        message: 'An error has occured',
+      });
+    }
+    if (timesheetArray.length === 0) {
+      return res.status(200).json({
+        message: 'Timesheet found',
+        data: TimesheetFound,
+      });
+    }
+    let filterByParams;
+    if (req.query.description) {
+      filterByParams = TimesheetFound.filter((timesheet) => timesheet.description
+    === req.query.description);
+    }
+    if (req.query.date) {
+      filterByParams = TimesheetFound.filter((timesheet) => timesheet.date
+    === req.query.date);
+    }
+    if (req.query.task) {
+      filterByParams = TimesheetFound.filter((timesheet) => timesheet.task === req.query.task);
+    }
+    return res.status(200).json({ filterByParams });
+  } catch (error) {
+    return res.json({
+      message: `An error has ocurred: ${error}`,
+    });
+  }
+};
+
 export default {
   createTimesheet,
+  getAllTimesheets,
 };
