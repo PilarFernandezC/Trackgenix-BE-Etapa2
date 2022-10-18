8931c317870ca1b759f7dd0cbdb41ec7f12de082
martinzancocchia <tinchozan@gmail.com>
2022-10-18 18:42:03 -0300

    TG-10: Schema and validations has been modified

--- src/models/Task.js
+++ src/models/Task.js
@@ -3,9 +3,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const taskSchema = new Schema({
-  description: {
-    type: String,
-  },
+    description: {type: String, required: true, enum: ['BE', 'FE']},
 });
 
-export default mongoose.model('Task', taskSchema);
+export default mongoose.model('Tasks', taskSchema);
--- src/validations/task.js
+++ src/validations/task.js
@@ -1,21 +1,20 @@
 import Joi from 'joi';
 
-const taskValidateUpdate = Joi.object({
-  description: Joi.string().valid(
-    'BE',
-    'FE',
-  ).required(),
-});
-
 const updateTaskValidation = (req, res, next) => {
-  const validation = taskValidateUpdate.validate(req.body);
-  if (validation.error) {
-    return res.status(400).json({
-      msg: 'There was a validation error:',
-      data: validation.error.details.map((x) => x.message).join(', '),
-      error: true,
+    const taskValidation = Joi.object({
+        description: Joi.string().valid('BE', 'FE').required(),
     });
-  } return next();
+
+    const validation = taskValidation.validate(req.body);
+
+    if (validation.error) {
+        return res.status(400).json({
+            message: `There was an error: ${validation.error.details[0].message}`,
+            error: true
+        });
+    }
+    return next();
 };
+
 
 export default updateTaskValidation;
