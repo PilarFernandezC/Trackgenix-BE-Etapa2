a6c680ffafb19ed178b1df6544ba29cd6067152a
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-10-18 20:44:32 -0300

    TG-17: Created projects endpoint GET method controller function.

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,6 +1,24 @@
-function dummyDBgetOne(req, res) {
-  res.send('dummyDBgetOne');
-}
+import Project from '../models/Projects';
+
+const dummyDBgetOne = async (req, res) => {
+  if (req.params.id) {
+    try {
+      const retrievedProject = await Project.findById(req.params.id);
+      res.status(200).json({
+        message: `Project with id=${req.params.id} found.`,
+        data: retrievedProject,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  } else {
+    res.status(400).json({
+      message: 'Error: no project id provided',
+    });
+  }
+};
 
 function dummyDBupdate(req, res) {
   res.send('dummyDBupdate');
