dad44406659cb70e0cf36e257102f820d770120d
Ernesto Bessone <ejubessone@gmail.com>
2022-10-18 11:33:17 -0300

    TG-13: Schema export name updated alongside the employee route

--- src/validations/employee.js
+++ src/validations/employee.js
@@ -1,44 +1,45 @@
 import Joi from 'joi';
 
-const createValidation = (req, res, next) => {
-  const employeeValidation = Joi.object({
-    name: Joi.string()
-      .required()
-      .pattern(/^[\p{L}]+$/u)
-      .min(3)
-      .messages({
-        'string.pattern.base': 'For property \'name\' : \'{name}\' all characters must be letters.',
-      }),
-    lastName: Joi.string()
-      .required()
-      .pattern(/^[\p{L}]+$/u)
-      .min(3)
-      .messages({
-        'string.pattern.base': 'For property \'lastName\' : \'{lastName}\' all characters must be letters.',
-      }),
-    phone: Joi.string()
-      .required()
-      .pattern(/^[0-9]+$/)
-      .min(9)
-      .messages({
-        'string.pattern.base': 'For property \'phone\' : \'{phone}\' all characters must be digits.',
-      }),
-    email: Joi.string()
-      .required()
-      .email(),
-    password: Joi.string()
-      .required()
-      .alphanum()
-      .min(8),
-  });
-  const validation = employeeValidation.validate(req.body);
+const employeeJoiSchema = Joi.object({
+  name: Joi.string()
+    .required()
+    .pattern(/^[\p{L}]+$/u)
+    .min(3)
+    .messages({
+      'string.pattern.base':
+        'For property \'name\' : \'{name}\' all characters must be letters.',
+    }),
+  lastName: Joi.string()
+    .required()
+    .pattern(/^[\p{L}]+$/u)
+    .min(3)
+    .messages({
+      'string.pattern.base': 'For property \'lastName\' : \'{lastName}\' all characters must be letters.',
+    }),
+  phone: Joi.string()
+    .required()
+    .pattern(/^[0-9]+$/)
+    .min(9)
+    .messages({
+      'string.pattern.base': 'For property \'phone\' : \'{phone}\' all characters must be digits.',
+    }),
+  email: Joi.string()
+    .required()
+    .email(),
+  password: Joi.string()
+    .required()
+    .pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
+});
 
-  if (validation.error) {
-    return res.status(400).json({
-      message: `Error: ${validation.error.details[0].message}`,
+const employeeValidatorMiddleware = async (req, res, next) => {
+  try {
+    await employeeJoiSchema.validateAsync(req.body);
+    next();
+  } catch (error) {
+    res.status(400).json({
+      message: `Data could not be validated: ${error.message}`,
     });
   }
-  return next();
 };
 
-export default { createValidation };
+export default employeeValidatorMiddleware;
