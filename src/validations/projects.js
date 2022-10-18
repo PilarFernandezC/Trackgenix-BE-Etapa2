ef592d43cde96ef5c533965541a81a85001215e0
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-10-18 19:51:41 -0300

    TG-17: Created Joi validation middleware

--- src/validations/projects.js
+++ src/validations/projects.js
@@ -1,7 +1,25 @@
-function dummyValidator(req, res, next) {
-  next();
-}
+import Joi from 'joi';
 
-export default {
-  update: dummyValidator,
+const createValidation = (req, res, next) => {
+  const superAdminValidation = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    starDate: Joi.date().required(),
+    endDate: Joi.date().greater('now').required(),
+    clientName: Joi.string().required(),
+    employee: [{
+      employeeId: Joi.string().required(),
+      role: Joi.string().valid('DEV', 'PM', 'QA', 'TL').required(),
+      rate: Joi.number().required(),
+    }],
+  });
+  const validation = superAdminValidation.validate(req.body);
+  if (validation.error) {
+    return res.status(400).json({
+      message: `There was an error ${validation.error.details[0].message}`,
+    });
+  }
+  return next();
 };
+
+export default { createValidation };
