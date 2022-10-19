d82677bf5275bc7d54abd2dee22d5caf3e09be65
martinzancocchia <tinchozan@gmail.com>
2022-10-19 17:33:52 -0300

    TG-15: merging

--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@ import mongoose from 'mongoose';
 import routes from './routes/index';
 
 // use "require" to import JSON files
-const tasksRouter = require('./resources/tasks');
-const employees = require('./resources/employees');
-const superAdmin = require('./resources/super-admins');
-const projectsRouter = require('./resources/projects');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/projects', projectsRouter);
-app.use('/tasks', tasksRouter);
-app.use('/timeSheets', timeSheetsRouter);
-app.use('/employees', employees);
-app.use('/superAdmin', superAdmin);
 app.use('/api', routes);
 
 app.get('/', async (req, res) => {
