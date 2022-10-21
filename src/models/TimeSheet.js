ba803fa8c2f8ee505d7c91a56e16c06c6726f10e
martinzancocchia <tinchozan@gmail.com>
2022-10-21 17:11:45 -0300

    TG-39: timesheet schema updated

--- src/models/TimeSheet.js
+++ src/models/TimeSheet.js
@@ -3,18 +3,30 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const timesheetSchema = new Schema({
+  date: { type: Date, required: true },
   description: {
     type: String,
     required: true,
   },
-  date: {
-    type: Date,
+  hours: {
+    type: Number,
     required: true,
   },
   task: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
     required: true,
+    ref: 'Task',
+  },
+  employee: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Employee',
+  },
+  project: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Project',
   },
 });
 
-export default mongoose.model('Timesheet', timesheetSchema);
+export default mongoose.model('TimeSheet', timesheetSchema);
