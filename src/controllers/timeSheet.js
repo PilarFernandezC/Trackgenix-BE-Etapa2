fab33b8bdafd9ac872e4be4b049846e9035ed34f
martinzancocchia <tinchozan@gmail.com>
2022-10-21 17:09:59 -0300

    TG-39: timesheet controller updated

--- src/controllers/timeSheet.js
+++ src/controllers/timeSheet.js
@@ -1,4 +1,4 @@
-import TimesheetModel from '../models/TimeSheet';
+import TimesheetModel from "../models/TimeSheet";
 
 const createTimesheet = async (req, res) => {
   try {
@@ -6,15 +6,18 @@ const createTimesheet = async (req, res) => {
       description: req.body.description,
       date: req.body.date,
       task: req.body.task,
+      employee: req.body.employee,
+      project: req.body.project,
+      hours: req.body.hours,
     });
     const result = await newTimesheet.save();
     return res.status(201).json({
-      message: 'Timesheet created',
+      message: "Timesheet created",
       data: result,
     });
   } catch (error) {
-    return res.status(400).json({
-      message: `An error has ocurred: ${error}`,
+    return res.status(error.status || 500).json({
+      message: error.message || error,
     });
   }
 };
@@ -22,34 +25,59 @@ const createTimesheet = async (req, res) => {
 const getAllTimesheets = async (req, res) => {
   const timesheetArray = Object.keys(req.query);
   try {
-    const TimesheetFound = await TimesheetModel.find();
+    const TimesheetFound = await TimesheetModel.find()
+      .populate("task")
+      .populate("employee")
+      .populate("project");
+
     if (!TimesheetFound) {
-      return res.status(404).json({
-        message: 'An error has occured',
-      });
+      // eslint-disable-next-line no-throw-literal
+      throw {
+        message: "Timesheet not found",
+        status: 404,
+      };
     }
     if (timesheetArray.length === 0) {
       return res.status(200).json({
-        message: 'Timesheet found',
+        message: "Timesheet found",
         data: TimesheetFound,
       });
     }
     let filterByParams;
     if (req.query.description) {
-      filterByParams = TimesheetFound.filter((timesheet) => timesheet.description
-    === req.query.description);
+      filterByParams = TimesheetFound.filter(
+        (timesheet) => timesheet.description === req.query.description
+      );
     }
     if (req.query.date) {
-      filterByParams = TimesheetFound.filter((timesheet) => timesheet.date
-    === req.query.date);
+      filterByParams = TimesheetFound.filter(
+        (timesheet) => timesheet.date === req.query.date
+      );
     }
     if (req.query.task) {
-      filterByParams = TimesheetFound.filter((timesheet) => timesheet.task === req.query.task);
+      filterByParams = TimesheetFound.filter(
+        (timesheet) => timesheet.task === req.query.task
+      );
     }
+    if (req.query.task) {
+      filterByParams = TimesheetFound.filter(
+        (timesheet) => timesheet.employee === req.query.employees
+      );
+    }
+    if (req.query.task) {
+      filterByParams = TimesheetFound.filter(
+        (timesheet) => timesheet.project === req.query.project
+      );
+    }
+    if (req.query.task) {
+        filterByParams = TimesheetFound.filter(
+          (timesheet) => timesheet.hours === req.query.hours
+        );
+      }
     return res.status(200).json({ filterByParams });
   } catch (error) {
-    return res.json({
-      message: `An error has ocurred: ${error}`,
+    return res.status(error.status || 500).json({
+      message: error.message || error,
     });
   }
 };
@@ -57,19 +85,25 @@ const getAllTimesheets = async (req, res) => {
 const getOneTimesheet = async (req, res) => {
   try {
     const { id } = req.params;
-    const timeSheet = await TimesheetModel.findById(id);
+    const timeSheet = await TimesheetModel.findById(id)
+      .populate("task")
+      .populate("employee")
+      .populate("project");
+
     if (!timeSheet) {
-      return res.status(404).json({
-        msg: 'The time-sheet has not been found',
-      });
+      // eslint-disable-next-line no-throw-literal
+      throw {
+        message: "Timesheet not found",
+        status: 404,
+      };
     }
     return res.status(200).json({
-      msg: 'The time-sheet has been found',
+      msg: "The time-sheet has been found",
       data: timeSheet,
     });
   } catch (error) {
-    return res.json({
-      message: `an error ocurred: ${error}`,
+    return res.status(error.status || 500).json({
+      message: error.message || error,
     });
   }
 };
@@ -82,18 +116,23 @@ const updateTimesheet = async (req, res) => {
       description: timeSheet.description,
       date: timeSheet.date,
       task: timeSheet.task,
+      employee: timeSheet.employee,
+      project: timeSheet.project,
+      hours: timeSheet.hours,
     });
     if (!response) {
-      return res.status(404).json({
-        msg: 'The time-sheet has not been found',
-      });
+      // eslint-disable-next-line no-throw-literal
+      throw {
+        message: "Timesheet not found",
+        status: 404,
+      };
     }
     return res.status(200).json({
-      msg: 'The time-sheet has been Updated',
+      msg: "The time-sheet has been Updated",
     });
   } catch (error) {
-    return res.json({
-      message: `an error ocurred: ${error}`,
+    return res.status(error.status || 500).json({
+      message: error.message || error,
     });
   }
 };
@@ -103,17 +142,19 @@ const deleteTimesheet = async (req, res) => {
     const { id } = req.params;
     const result = await TimesheetModel.findByIdAndDelete(id);
     if (!result) {
-      return res.status(404).json({
-        msg: 'The time-sheet has not been found',
-      });
+      // eslint-disable-next-line no-throw-literal
+      throw {
+        message: "Timesheet not found",
+        status: 404,
+      };
     }
     return res.status(204).json({
-      msg: 'The time-sheet has been deleted: ',
+      msg: "The time-sheet has been deleted: ",
       data: result,
     });
   } catch (error) {
-    return res.json({
-      message: `an error ocurred: ${error}`,
+    return res.status(error.status || 500).json({
+      message: error.message || error,
     });
   }
 };
