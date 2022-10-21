ce7152bc255a1052dabc72bd3df0adb3eac7a910
PilarFernandezC <pilarfernandezc98@gmail.com>
2022-10-21 15:15:35 -0300

    TG-22: set up the environment

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-import express from 'express';
 import mongoose from 'mongoose';
-import routes from './routes/index';
+import app from './app';
 
-const app = express();
-const port = process.env.PORT || 3000;
+require('dotenv').config();
 
-app.use(express.json());
-app.use('/api', routes);
-
-app.get('/', async (req, res) => {
-  res.send('Hello World!');
-});
-
-const MONGO_URL = 'mongodb+srv://BaSP-database-ayom-a:BaSP2022@cluster0.b8vlcfc.mongodb.net/?retryWrites=true&w=majority';
+const port = process.env.PORT;
 
 mongoose.connect(
-  MONGO_URL,
+    process.env.MONGO_URL,
   (error) => {
     if (error) {
       console.log('Fails connection to database', error);
