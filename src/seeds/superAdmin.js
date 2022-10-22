5900cb38b3d3badbdc3386390d9a1a6d227d67d6
matiasraviola <matiasaraviola@gmail.com>
2022-10-22 16:11:18 -0300

    TG-31: superAdmin seeds updated

---
+++ src/seeds/superAdmin.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+
+export default [{
+  _id: mongoose.Types.ObjectId('63543d4cfc13ae204b000038'),
+  first_name: 'Aron',
+  last_name: 'Bretherick',
+  email: 'abretherick0@bluehost.com',
+  password: 'QsH73hT',
+}, {
+  _id: mongoose.Types.ObjectId('63543d4cfc13ae204b000039'),
+  first_name: 'Maddy',
+  last_name: 'Lincey',
+  email: 'mlincey1@cornell.edu',
+  password: 'Qvk23EHRkP',
+}, {
+  _id: mongoose.Types.ObjectId('63543d4cfc13ae204b00003a'),
+  first_name: 'Ozzie',
+  last_name: 'Dmitrievski',
+  email: 'odmitrievski2@sourceforge.net',
+  password: 'FMoizUbLi',
+}, {
+  _id: mongoose.Types.ObjectId('63543d4cfc13ae204b00003b'),
+  first_name: 'Jessee',
+  last_name: 'Quayle',
+  email: 'jquayle3@addthis.com',
+  password: 'OiG4q1dS',
+}, {
+  _id: mongoose.Types.ObjectId('63543d4cfc13ae204b00003c'),
+  first_name: 'Salaidh',
+  last_name: 'Mathison',
+  email: 'smathison4@wikispaces.com',
+  password: 'y8dSSDDDu',
+}, {
+  _id: mongoose.Types.ObjectId('63543d4cfc13ae204b00003d'),
+  first_name: 'Zacharie',
+  last_name: 'Lamb-shine',
+  email: 'zlambshine5@springer.com',
+  password: 'y9fW2G1b4s',
+}, {
+  _id: mongoose.Types.ObjectId('63543d4cfc13ae204b00003e'),
+  first_name: 'Emmery',
+  last_name: 'Dyball',
+  email: 'edyball6@cisco.com',
+  password: 'khmEBCEgfva',
+}, {
+  _id: mongoose.Types.ObjectId('63543d4cfc13ae204b00003f'),
+  first_name: 'Jakob',
+  last_name: 'Omand',
+  email: 'jomand7@unblog.fr',
+  password: 'gIRG0M',
+}, {
+  _id: mongoose.Types.ObjectId('63543d4cfc13ae204b000040'),
+  first_name: 'Locke',
+  last_name: 'Wannell',
+  email: 'lwannell8@infoseek.co.jp',
+  password: 'RTmdXm',
+}, {
+  _id: mongoose.Types.ObjectId('63543d4cfc13ae204b000041'),
+  first_name: 'Yancey',
+  last_name: "O' Faherty",
+  email: 'yofaherty9@army.mil',
+  password: 'M0g4cA',
+}];
