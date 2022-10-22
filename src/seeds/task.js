c9ba450eca08c1eb8f1c2e4b7fcfdd384e82536c
Luciano Franchino <luciano_franchino@hotmail.com>
2022-10-22 16:40:50 -0300

    TG-30: Create task seed

---
+++ src/seeds/task.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+export default [{
+  _id: mongoose.Types.ObjectId('63544114fc13ae2db7000330'),
+  description: 'Sed ante. Vivamus tortor. Duis mattis egestas metus.',
+}, {
+  _id: mongoose.Types.ObjectId('63544114fc13ae2db7000331'),
+  description: 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.',
+}, {
+  _id: mongoose.Types.ObjectId('63544114fc13ae2db7000332'),
+  description: 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.',
+}, {
+  _id: mongoose.Types.ObjectId('63544114fc13ae2db7000333'),
+  description: 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.',
+}, {
+  _id: mongoose.Types.ObjectId('63544114fc13ae2db7000334'),
+  description: 'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+}, {
+  _id: mongoose.Types.ObjectId('63544114fc13ae2db7000335'),
+  description: 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+}, {
+  _id: mongoose.Types.ObjectId('63544114fc13ae2db7000336'),
+  description: 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.',
+}, {
+  _id: mongoose.Types.ObjectId('63544114fc13ae2db7000337'),
+  description: 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
+}, {
+  _id: mongoose.Types.ObjectId('63544114fc13ae2db7000338'),
+  description: 'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.',
+}, {
+  _id: mongoose.Types.ObjectId('63544114fc13ae2db7000339'),
+  description: 'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.',
+}];
