61ee494c976cdab3f39241aadf8517d7e7cde5a9
Luciano Franchino <luciano_franchino@hotmail.com>
2022-10-23 12:24:51 -0300

    TG-30: correct task seed

--- src/seeds/task.js
+++ src/seeds/task.js
@@ -2,32 +2,32 @@ import mongoose from 'mongoose';
 
 export default [{
   _id: mongoose.Types.ObjectId('63544114fc13ae2db7000330'),
-  description: 'Sed ante. Vivamus tortor. Duis mattis egestas metus.',
+  description: 'FE',
 }, {
   _id: mongoose.Types.ObjectId('63544114fc13ae2db7000331'),
-  description: 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.',
+  description: 'FE',
 }, {
   _id: mongoose.Types.ObjectId('63544114fc13ae2db7000332'),
-  description: 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.',
+  description: 'BE',
 }, {
   _id: mongoose.Types.ObjectId('63544114fc13ae2db7000333'),
-  description: 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.',
+  description: 'BE',
 }, {
   _id: mongoose.Types.ObjectId('63544114fc13ae2db7000334'),
-  description: 'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+  description: 'FE',
 }, {
   _id: mongoose.Types.ObjectId('63544114fc13ae2db7000335'),
-  description: 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+  description: 'BE',
 }, {
   _id: mongoose.Types.ObjectId('63544114fc13ae2db7000336'),
-  description: 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.',
+  description: 'FE',
 }, {
   _id: mongoose.Types.ObjectId('63544114fc13ae2db7000337'),
-  description: 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
+  description: 'BE',
 }, {
   _id: mongoose.Types.ObjectId('63544114fc13ae2db7000338'),
-  description: 'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.',
+  description: 'FE',
 }, {
   _id: mongoose.Types.ObjectId('63544114fc13ae2db7000339'),
-  description: 'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.',
+  description: 'BE',
 }];
