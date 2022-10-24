f6b837e4c6ac56b392e2975660c0c6502d93d038
matiasraviola <matiasaraviola@gmail.com>
2022-10-24 17:34:15 -0300

    TG-35: DELETE test done

--- src/tests/timesheets.test.js
+++ src/tests/timesheets.test.js
@@ -1,1 +1,38 @@
-test('Delete this test', () => {});
+import request from 'supertest';
+import timesheet from '../models/TimeSheet';
+import app from '../app';
+import timeSheetSeed from '../seeds/timesheets';
+
+beforeAll(async () => {
+  await timesheet.collection.insertMany(timeSheetSeed);
+});
+
+const timesheetId = '6354438cfc13ae204b000060';
+
+// const mockedTimesheet ={
+//     description: 'vitae nisl aenean',
+//     date: '1/1/2022',
+//     task: mongoose.Types.ObjectId('63546010fc13ae3a75000196'),
+//     employee: mongoose.Types.ObjectId('6354438cfc13ae204b000061'),
+//     project: mongoose.Types.ObjectId('6354438cfc13ae204b000062'),
+//     hours: 81
+// }
+
+describe('DELETE /timesheet/:id', () => {
+  test('should delete an employee', async () => {
+    const response = await request(app).delete(`/api/timesheets/${timesheetId}`).send();
+    expect(response.status).toBe(204);
+  });
+  test("if I don't pass the id the response should be a status 404", async () => {
+    const response = await request(app).delete('/api/timesheets/').send();
+    expect(response.status).toBe(404);
+  });
+  test('response status should be 404 with a wrong route ', async () => {
+    const response = await request(app).delete(`/api/timesheet/${timesheetId}`).send();
+    expect(response.status).toBe(404);
+  });
+  test('response status should be 404 with a wrong id', async () => {
+    const response = await request(app).delete('/api/timesheets/6356efc2fc13ae56b9000014').send();
+    expect(response.status).toBe(404);
+  });
+});
