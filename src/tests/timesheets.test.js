fdd4bfaac5fb1ed8b754ee23d87dfb96445ba3c5
matiasraviola <matiasaraviola@gmail.com>
2022-10-24 20:39:45 -0300

    TG-35: fix an error in /controllers on the hours filter and GET test

--- src/tests/timesheets.test.js
+++ src/tests/timesheets.test.js
@@ -8,6 +8,14 @@ beforeAll(async () => {
 });
 
 const timesheetId = '6354438cfc13ae204b000060';
+const timesheetDesc = 'vitae nisl aenean';
+const timesheetDate = '1/1/2022';
+const timesheetTask = '63546010fc13ae3a75000196';
+const timesheetEmployee = '6354438cfc13ae204b000061';
+const timesheetProject = '6354438cfc13ae204b000062';
+const timesheetHours = 81;
+
+// const timesheetInvalidHours = 'asffgasd';
 
 // const mockedTimesheet ={
 //     description: 'vitae nisl aenean',
@@ -35,4 +43,54 @@ describe('DELETE /timesheet/:id', () => {
     const response = await request(app).delete('/api/timesheets/6356efc2fc13ae56b9000014').send();
     expect(response.status).toBe(404);
   });
+});
+describe('GET /timesheet', () => {
+  test('should find all the timesheets', async () => {
+    const response = await request(app).get('/api/timesheets').send();
+    expect(response.status).toBe(200);
+  });
+  test('should not find all the timesheets with a wrong route', async () => {
+    const response = await request(app).get('/api/timesheet').send();
+    expect(response.status).toBe(404);
+  });
+  test('should find the timesheets filter by description', async () => {
+    const response = await request(app).get(`/api/timesheets/?description=${timesheetDesc}`).send();
+    expect(response.status).toBe(200);
+  });
+  test('should find the timesheets filter by date', async () => {
+    const response = await request(app).get(`/api/timesheets/?date=${timesheetDate}`).send();
+    expect(response.status).toBe(200);
+  });
+  test('should find the timesheets filter by task', async () => {
+    const response = await request(app).get(`/api/timesheets/?task=${timesheetTask}`).send();
+    expect(response.status).toBe(200);
+  });
+  test('should find the timesheets filter by employee', async () => {
+    const response = await request(app).get(`/api/timesheets/?employee=${timesheetEmployee}`).send();
+    expect(response.status).toBe(200);
+  });
+  test('should find the timesheets filter by project', async () => {
+    const response = await request(app).get(`/api/timesheets/?project=${timesheetProject}`).send();
+    expect(response.status).toBe(200);
+  });
+  test('should find the timesheets filter by hours', async () => {
+    const response = await request(app).get(`/api/timesheets/?hours=${timesheetHours}`).send();
+    expect(response.status).toBe(200);
+  });
+
+  // test('should not find the timesheets with a invalid hours type', async () => {
+  //     const response = await request(app).get(`/api/timesheets/?hours=${timesheetInvalidHours}`)
+  // .send();
+  //     expect(response.status).toBe(404);
+  // });
+  // test('should find the timesheets filter by hours', async () => {
+  //     const response = await request(app).get(`/api/timesheets/?hours=10000`).send();
+  //     expect(response.status).toBe(404);
+  // });
+
+  // test('should find the timesheets filter by description', async () => {
+  //     const response = await request(app).get(`/api/timesheets/?description=esta desc no exite`)
+//   .send();
+  //     expect(response.status).toBe(404);
+  // });
 });
