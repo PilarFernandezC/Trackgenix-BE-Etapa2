2d6e9d273bdc78333549df97a84df0c3ea105588
Luciano Franchino <luciano_franchino@hotmail.com>
2022-10-24 20:49:56 -0300

    TG-29: projects test correct

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import routes from './routes/index';
 const app = express();
 
 app.use(express.json());
-app.use('/api', routes);
+app.use('/', routes);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
--- src/tests/projects.test.js
+++ src/tests/projects.test.js
@@ -1,1 +1,42 @@
-test('Delete this test', () => {});
+import request from 'supertest';
+import app from '../app';
+import Projects from '../models/Projects';
+import projectsSeed from '../seeds/projects';
+
+const idProject = '635015885581eb421df09402';
+const idInvalid = '635015885581eb421df09404';
+
+describe('Projects - Unit test', () => {
+});
+
+beforeAll(async () => {
+  await Projects.collection.insertMany(projectsSeed);
+});
+
+describe('GET /projects', () => {
+  test('should return status code 200', async () => {
+    const response = await request(app).get('/projects').send();
+    expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.data).toBeDefined();
+    expect(response.body.message).toBe('Projects found');
+  });
+});
+
+describe('GET BY ID /:id', () => {
+  test('should return status code 200', async () => {
+    const response = await request(app).get(`/projects/${idProject}`).send();
+    expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.data).toBeDefined();
+  });
+});
+
+describe('GET BY ID ERROR /:id', () => {
+  test('should return status code 400', async () => {
+    const response = await request(app).get(`/projects/${idInvalid}`).send();
+    expect(response.status).toBe(404);
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.message).toBe(`${response.body.message}`);
+  });
+});
