4f23af16d8336827193a2992684f1f6230fa540b
matiasraviola <matiasaraviola@gmail.com>
2022-10-25 20:06:27 -0300

    TG-24: mockedadmin object tested in the getAll filter

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -7,7 +7,9 @@ beforeAll(async () => {
   await admin.collection.insertMany(admins);
 });
 
+// const adminId = '635437ebfc13ae2db70002b8'
 const mockedAdmin = {
+  _id: '635437ebfc13ae2db70002b8',
   name: 'Tye',
   lastName: 'Haysar',
   email: 'thaysar0@miitbeian.gov.cn',
@@ -15,21 +17,22 @@ const mockedAdmin = {
 };
 
 describe('GET /admin', () => {
-  test('should find all the admins', async () => {
+  test('should get all the admins', async () => {
     const response = await request(app).get('/api/admin').send();
+    expect(response.body.data).toBeDefined();
     expect(response.status).toBe(200);
   });
-  test('should not find all the admins with a wrong route', async () => {
-    const response = await request(app).get('/api/admins').send();
+  test('should not get all the admins with a wrong route', async () => {
+    const response = await request(app).get(`/api/admins/?name=${mockedAdmin.name}`).send();
     expect(response.status).toBe(404);
   });
 
   test('should find the admins filter by description', async () => {
-    const response = await request(app).get(`/api/admin/?name=${mockedAdmin}`).send();
+    const response = await request(app).get(`/api/admin/?name=${mockedAdmin.lastName}`).send();
     expect(response.status).toBe(200);
   });
   test('should find the admins filter by date', async () => {
-    const response = await request(app).get(`/api/admin/?lastName=${mockedAdmin}`).send();
+    const response = await request(app).get(`/api/admin/?lastName=${mockedAdmin.email}`).send();
     expect(response.status).toBe(200);
   });
 
