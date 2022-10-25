472d029d0fc1fd3458924aff2656328aff2fa9cf
matiasraviola <matiasaraviola@gmail.com>
2022-10-25 19:57:35 -0300

    TG-24: seed.js fields correction and getAll test

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -1,1 +1,40 @@
-test('Delete this test', () => {});
+import request from 'supertest';
+import admin from '../models/Admin';
+import app from '../app';
+import admins from '../seeds/admins';
+
+beforeAll(async () => {
+  await admin.collection.insertMany(admins);
+});
+
+const mockedAdmin = {
+  name: 'Tye',
+  lastName: 'Haysar',
+  email: 'thaysar0@miitbeian.gov.cn',
+  password: 'E6HCIX',
+};
+
+describe('GET /admin', () => {
+  test('should find all the admins', async () => {
+    const response = await request(app).get('/api/admin').send();
+    expect(response.status).toBe(200);
+  });
+  test('should not find all the admins with a wrong route', async () => {
+    const response = await request(app).get('/api/admins').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('should find the admins filter by description', async () => {
+    const response = await request(app).get(`/api/admin/?name=${mockedAdmin}`).send();
+    expect(response.status).toBe(200);
+  });
+  test('should find the admins filter by date', async () => {
+    const response = await request(app).get(`/api/admin/?lastName=${mockedAdmin}`).send();
+    expect(response.status).toBe(200);
+  });
+
+  test('should find the admins filter by task', async () => {
+    const response = await request(app).get(`/api/admin/?email=${mockedAdmin}`).send();
+    expect(response.status).toBe(200);
+  });
+});
