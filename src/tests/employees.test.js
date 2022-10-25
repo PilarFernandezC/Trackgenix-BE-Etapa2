17d9757128c7c98a7965d4b61561ebc273618d25
Ernesto Bessone <ejubessone@gmail.com>
2022-10-25 19:53:06 -0300

    TG-36: getById employees test added

--- src/tests/employees.test.js
+++ src/tests/employees.test.js
@@ -7,6 +7,7 @@ beforeAll(async () => {
   await Employee.collection.insertMany(employeeSeed);
 });
 
+const employeeInvalidId = '6354389ffc13ae2db7004444';
 const employeeUser = {
   _id: '6354389ffc13ae2db7000326',
   name: 'Isahella',
@@ -69,5 +70,24 @@ describe('getAll /employees', () => {
       .send();
     expect(response.status).toBe(404);
     expect(response.status.error).toBeUndefined();
+  });
+});
+
+describe('getById /employees', () => {
+  test('should get an employee by ID', async () => {
+    const response = await request(app)
+    // eslint-disable-next-line no-underscore-dangle
+      .get(`/api/employees/${employeeUser._id}`)
+      .send();
+    expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.data).toEqual(employeeUser);
+  });
+  test('should not find an employee with the ID given thus return error 404', async () => {
+    const response = await request(app)
+      .get(`/api/employees/${employeeInvalidId}`)
+      .send();
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBeUndefined();
   });
 });
