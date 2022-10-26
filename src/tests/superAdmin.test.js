84f578d4df538738204dbf08da8fe3982137a272
Luciano Franchino <luciano_franchino@hotmail.com>
2022-10-25 22:06:58 -0300

    TG-33: edit superAdmin controllers and create superAdmin getAll test

--- src/tests/superAdmin.test.js
+++ src/tests/superAdmin.test.js
@@ -1,1 +1,18 @@
-test('Delete this test', () => {});
+import request from 'supertest';
+import app from '../app';
+import SuperAdmin from '../models/SuperAdmin';
+import superAdminSeeds from '../seeds/superAdmin';
+
+beforeAll(async () => {
+  await SuperAdmin.collection.insertMany(superAdminSeeds);
+});
+
+describe('GET /superAdmin', () => {
+  test('should return status code 200', async () => {
+    const response = await request(app).get('/api/superAdmin/').send();
+    expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.data).toBeDefined();
+    expect(response.body.message).toBe('SuperAdmins found');
+  });
+});
