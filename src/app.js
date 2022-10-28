a03474a4f917c6217ca2834533cecc7eed16330e
Cris-Juli <cristianivanbucci@gmail.com>
2022-10-28 13:07:30 -0300

    TG-48:Install cors library

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', routes);
 
 app.get('/', async (req, res) => {
