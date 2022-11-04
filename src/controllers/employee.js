a748c5ce7dde1f4226882f4fe8872c6cfcbb838d
Mateo Scarabino <91098568+matescarabino@users.noreply.github.com>
2022-11-04 14:26:27 -0300

    TG-55: fix typo error in timeSheet controller

--- src/controllers/employee.js
+++ src/controllers/employee.js
@@ -86,10 +86,9 @@ const editEmployee = async (req, res) => {
       { new: true },
     );
     if (!employee) {
-      // eslint-disable-next-line no-throw-literal
-      throw {
-        message: 'Employee not found', status: 404,
-      };
+      return res.status(404).json({
+        message: 'Employee not found',
+      });
     }
     return res.status(200).json({
       message: `Employee with the ID ${req.params.id} has been updated.`,
