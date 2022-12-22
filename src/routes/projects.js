fad70d40fe37bc8cfea8958bc66b731a9710879f
Mateo Scarabino <91098568+matescarabino@users.noreply.github.com>
2022-12-22 01:37:27 -0300

    TG-163: add role employee to project put route

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/', checkAuth([ADMIN, EMPLOYEE]), dbController.getAllProjects);
 router.get('/:id', checkAuth([ADMIN, EMPLOYEE]), dbController.getProjectById);
 router.post('/', checkAuth([ADMIN]), dataValidator, dbController.createProject);
-router.put('/:id', checkAuth([ADMIN]), dataValidator, dbController.editProject);
+router.put('/:id', checkAuth([ADMIN, EMPLOYEE]), dataValidator, dbController.editProject);
 router.delete('/:id', checkAuth([ADMIN]), dbController.deleteById);
 
 export default router;
